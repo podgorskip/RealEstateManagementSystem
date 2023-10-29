505bae4c5b95a484ab600969d906e56c0c2c7adb
Patryk Podgórski <podgorski.p@interia.eu>
2023-10-29 18:35:53 +0100

    Adds properties foe admin entity and removes setters for some fields

--- src/main/java/podgorskip/managementSystem/jpa/entities/Administrator.java
+++ src/main/java/podgorskip/managementSystem/jpa/entities/Administrator.java
@@ -1,7 +1,9 @@
 package podgorskip.managementSystem.jpa.entities;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 import java.util.Date;
 
@@ -9,6 +11,7 @@ import java.util.Date;
 @Table(name = "admins", schema = "real_estate")
 @Data
 public class Administrator {
+    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -16,9 +19,17 @@ public class Administrator {
     @Column(unique = true)
     private String username;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String password;
 
+    @Setter(AccessLevel.NONE)
     private Date created;
 
+    @Setter(AccessLevel.NONE)
     private Role role = Role.ADMIN;
 }
