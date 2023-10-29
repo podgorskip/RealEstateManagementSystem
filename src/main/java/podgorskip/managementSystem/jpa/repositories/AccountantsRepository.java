34a6cdc210e52f956ca374e07fb92fbd67a5a9c7
Patryk Podgórski <podgorski.p@interia.eu>
2023-10-29 18:29:26 +0100

    Adds repositories for new entities

---
+++ src/main/java/podgorskip/managementSystem/jpa/repositories/AccountantsRepository.java
@@ -0,0 +1,9 @@
+package podgorskip.managementSystem.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import podgorskip.managementSystem.jpa.entities.Accountant;
+
+@Repository
+public interface AccountantsRepository extends JpaRepository<Accountant, Integer> {
+}
