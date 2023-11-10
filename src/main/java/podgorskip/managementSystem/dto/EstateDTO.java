70f2fb04c25ad126387fbda6d7cf0d3048420125
Patryk Podgórski <podgorski.p@interia.eu>
2023-11-10 17:51:33 +0100

    Implements method that checks if required attributed from user input are not null

--- src/main/java/podgorskip/managementSystem/dto/EstateDTO.java
+++ src/main/java/podgorskip/managementSystem/dto/EstateDTO.java
@@ -3,21 +3,28 @@ package podgorskip.managementSystem.dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Data
 public class EstateDTO {
-    private int id;
+    private Integer id;
     private String description;
     private String type;
-    private int price;
-    private int rooms;
-    private int bathrooms;
+    private Integer price;
+    private Integer rooms;
+    private Integer bathrooms;
     private String localization;
-    private boolean garage;
-    private int storeys;
+    private Boolean garage;
+    private Integer storeys;
     private Date published;
     private String agentFirstName;
     private String agentLastName;
     private String agentEmail;
     private String agentPhoneNumber;
+
+    public boolean validateData() {
+        return Objects.nonNull(id) && Objects.nonNull(description) && Objects.nonNull(type) &&
+                Objects.nonNull(price) && Objects.nonNull(rooms) && Objects.nonNull(bathrooms) &&
+                Objects.nonNull(localization) && Objects.nonNull(garage) && Objects.nonNull(storeys);
+    }
 }
