4890cdef4b412007377f6ef73785faf46ebba8eb
Patryk Podgórski <podgorski.p@interia.eu>
2023-11-29 14:04:17 +0100

    Enables aspects

--- src/main/java/podgorskip/managementSystem/WebConfiguration.java
+++ src/main/java/podgorskip/managementSystem/WebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -31,6 +32,7 @@ import podgorskip.managementSystem.utils.LoggerInterceptor;
 @EnableWebSecurity
 @EnableJpaRepositories(basePackages = "podgorskip.managementSystem.jpa")
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 @EntityScan(basePackages = "podgorskip.managementSystem.jpa")
 @ComponentScan
 public class WebConfiguration {
